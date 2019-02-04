054c010a11b0ffaebc01deacd8cd30bb98e207bd
Steffon Williams <steffonwilliams@Steffons-MacBook-Air.local>
2019-02-04 15:51:14 -0500

    started Entity Class for User

---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,38 @@
+package com.example.demo.domain;
+
+import rocks.zipcode.io.config.Constants;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import sun.rmi.rmic.Constants;
+
+import javax.validation.constraints.Email;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Set;
+import java.time.Instant;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(length = 50, unique = true, nullable = false)
+    private String login;
+
+
+}
